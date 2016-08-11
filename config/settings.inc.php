4d6ed7b86a33050031d799e8d127890f8749f219
alumno <alumno>
2016-08-11 12:21:17 -0500

    terminado2

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -1,8 +1,8 @@
 <?php
-define('_DB_SERVER_', 'localhost');
-define('_DB_NAME_', 'tienda3');
-define('_DB_USER_', 'root');
-define('_DB_PASSWD_', '');
+define('_DB_SERVER_', 'mysql.hostinger.mx');
+define('_DB_NAME_', 'u636275184_t3');
+define('_DB_USER_', 'u636275184_ome');
+define('_DB_PASSWD_', 'omelino05');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
